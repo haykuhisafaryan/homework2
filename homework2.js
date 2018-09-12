fbbc909b9ec21477613de5a55df4d9c7d2c3f9cc
Your Name <you@example.com>
2018-09-12 05:07:27 -0700

    Hello

---
+++ homework2.js
@@ -0,0 +1,27 @@
+
+const WineType = "Chardonnay";
+const WineColor="White";
+const winePrice=25
+const WineType1 = "Pinot Noir";
+const WineColor1="Red";
+const winePrice1=30;
+
+
+console.log((200-40)*6);
+console.log((200/winePrice)*76);
+console.log(winePrice !== 7+9)
+
+
+
+
+
+if(WineColor === "White"){
+	console.log( "You need to pay 25$ per bottle");
+} else { console.log( "Then you should pay 30$ for  bottle of red wine");
+}
+
+
+if(WineColor1 ==="Red"){
+	console.log( "We can suggest you Pinot Noir");
+} else { console.log( "My recommendation will be Chardonnay ") 
+}
